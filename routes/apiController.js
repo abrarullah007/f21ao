b9578af9a82b55361041481928d29f2443c9172a
drabrar <abrar.ullah@gmail.com>
2021-02-26 12:08:38 +0400

    more changes

--- routes/apiController.js
+++ routes/apiController.js
@@ -2,7 +2,12 @@ class apiController {
   static async index(req, res) {
     try {
 		 var user = req.query.username;
-res.send("Welcome %s to home page", user);
+ var str = "Hello "+ user +"! Welcome to home page";
+			res.send(str);
+  } catch (exception) {
+      res.status(500).send(exception);
+    }
+  }
 }
 
 module.exports = apiController;
