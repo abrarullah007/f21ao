2e6986a06a8c1a4fc0bbf349f71f3dc24c451016
Abrar <abrar.ullah@gmail.com>
2024-03-04 12:57:47 +0400

    changes

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
+ const bodyParser = require("body-parser");
+ // const dotenv = require("dotenv");
+ // dotenv.config();
+ const port = process.env.PORT || 8080;
+ // create application/x-www-form-urlencoded parser
+ app.use(bodyParser.urlencoded({ extended: false }));
+ // create application/json parser
+ // app.use(bodyParser.json());
+ // app.get("/", function (req, res){
+ //   res.send("welcome to home page!");
+ // })
+ // Move all routes to routes.js
+ require("./routes/routes.js")(app);
+ // console.log(process.env.check, process.env.check2);
 
-const port = 8088;
-// create application/x-www-form-urlencoded parser
-app.use(bodyParser.urlencoded({ extended: false }));
-// create application/json parser
-app.use(bodyParser.json());
-// app.get("/", function (req, res){
-//   res.send("welcome to home page!");
-// })
-// Move all routes to routes.js
-require("./routes/routes.js")(app);
-
-const server = app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+ const server = app.listen(port, () => {
+   console.log(`Example app listening at :${port}`);
+ });
 
 module.exports = server;
