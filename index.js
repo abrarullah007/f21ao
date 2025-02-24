3ba688fc9ab6b35ef347103a07b59e6d6587c6da
Abrar <abrar.ullah@gmail.com>
2025-02-24 18:27:37 +0400

    recent changes

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ const app = express();
  const bodyParser = require("body-parser");
  // const dotenv = require("dotenv");
  // dotenv.config();
- const port = process.env.PORT || 8080;
+ const port = process.env.PORT || 8088;
  // create application/x-www-form-urlencoded parser
  app.use(bodyParser.urlencoded({ extended: false }));
  // create application/json parser
- // app.use(bodyParser.json());
- // app.get("/", function (req, res){
- //   res.send("welcome to home page!");
- // })
+ app.use(bodyParser.json());
+//  app.get("/", function (req, res){
+//    res.send("welcome to home page!");
+//  });
+//  app.get("/users", function(req,res) {
+//   let users =[{id:"1", name:"abrar"}, {id:"2", name: "Joe Blogg"}];
+//   res.send(JSON.stringify(users));
+//  })
  // Move all routes to routes.js
  require("./routes/routes.js")(app);
  // console.log(process.env.check, process.env.check2);
